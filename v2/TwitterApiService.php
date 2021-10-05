94db54fac01c6373381220b94779e384092e8fea
xxbbxx825 <xxbbxx825@gmail.com>
2021-10-05 11:27:27 +0900

    add TwitterApiService v2

---
+++ v2/TwitterApiService.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+namespace TwitterApiService;
+
+require_once "./vendor/autoload.php";
+require_once './Credential.php';
+
+use Credential\Credential;
+use GuzzleHttp\Client;
+
+class TwitterApiService
+{
+    private $client;
+    private $bearToken;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $credential = new Credential;
+        $this->bearToken = $credential->getBearToken();
+    }
+
+    public function searchRecent(array $queryParameters)
+    {
+        $endPoint =  'https://api.twitter.com/2/tweets/search/recent';
+        $requests = [
+            'headers' => ['Authorization' => 'Bearer ' . $this->bearToken],
+            'query' => $queryParameters
+        ];
+        try {
+            $result = $this->client->request('GET', $endPoint, $requests)->getBody()->getContents();
+            return json_decode($result);
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function getTweets(array $queryParameters)
+    {
+        $endPoint =  'https://api.twitter.com/2/tweets';
+        $requests = [
+            'headers' => ['Authorization' => 'Bearer ' . $this->bearToken],
+            'query' => $queryParameters
+        ];
+        try {
+            $result = $this->client->request('GET', $requests)->getBody()->getContents();
+            $jsonResult = json_decode($result);
+            return $jsonResult;
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function postStreamRules(array $rules, bool $dryRun = false)
+    {
+        $endPoint =  'https://api.twitter.com/2/tweets/search/stream/rules';
+        $requests = [
+            'headers' => ['Authorization' => 'Bearer ' . $this->bearToken],
+            'query' => ['dry_run' => $dryRun],
+            'json' => ['add' => [$rules]]
+        ];
+        try {
+            $result = $this->client->request('POST', $endPoint, $requests)->getBody()->getContents();
+            $jsonResult = json_decode($result);
+            if ($jsonResult->meta->summary->created) {
+                return [
+                    'data' => $jsonResult->data,
+                    'success' => true,
+                ];
+            } else {
+                return [
+                    'message' => $jsonResult->errors,
+                    'success' => false,
+                ];
+            }
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
